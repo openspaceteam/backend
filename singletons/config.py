5f43b28b0a52a9b3f1987221131a717b0e5c1ffa
Giuseppe Guerra <to@nyo.zz.mu>
2018-01-12 23:27:46 +0100

    ???

--- singletons/config.py
+++ singletons/config.py
@@ -14,9 +14,9 @@ class Config:
             "SIO_PORT": config("SIO_PORT", default="4433", cast=int)
         }
 
-        if not self["DEBUG"]:
+        if not self._config["DEBUG"]:
             # Force debug options to off if debug mode is off
-            self["SINGLE_PLAYER"] = False
+            self._config["SINGLE_PLAYER"] = False
 
     def __getitem__(self, item):
         return self._config[item]
