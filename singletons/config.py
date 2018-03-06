c36f2f1a2a049a73beea5559da65fe9e9c712017
Giuseppe Guerra <to@nyo.zz.mu>
2018-03-06 21:01:36 +0100

    Add SSL support

--- singletons/config.py
+++ singletons/config.py
@@ -11,7 +11,10 @@ class Config:
             "SINGLE_PLAYER": config("SINGLE_PLAYER", default="0", cast=bool),
 
             "SIO_HOST": config("SIO_HOST", default="127.0.0.1"),
-            "SIO_PORT": config("SIO_PORT", default="4433", cast=int)
+            "SIO_PORT": config("SIO_PORT", default="4433", cast=int),
+
+            "SSL_CERT": config("SSL_CERT", default="cert.crt"),
+            "SSL_KEY": config("SSL_KEY", default="key.key"),
         }
 
         if not self._config["DEBUG"]:
--- spaceteam.py
+++ spaceteam.py
@@ -1,5 +1,7 @@
 import logging
+import ssl
 
+import os
 from aiohttp import web
 
 from singletons.config import Config
@@ -36,12 +38,25 @@ def main():
     app = web.Application()
     Sio().attach(app)
 
+    # Load SSL context
+    cert_path = Config()["SSL_CERT"]
+    key_path = Config()["SSL_KEY"]
+    if cert_path and key_path and os.path.isfile(cert_path) and os.path.isfile(key_path):
+        logging.info("Using SSL")
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        ssl_context.load_cert_chain(Config()["SSL_CERT"], Config()["SSL_KEY"])
+    else:
+        logging.warning("SSL is disabled!")
+        ssl_context = None
+
     # Start server
     web.run_app(
         app,
         host=Config()["SIO_HOST"],
-        port=Config()["SIO_PORT"]
+        port=Config()["SIO_PORT"],
+        ssl_context=ssl_context
     )
+
 
 if __name__ == '__main__':
     main()
